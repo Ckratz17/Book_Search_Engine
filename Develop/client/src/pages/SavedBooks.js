01e070ae868ae84e131693c71093db38585b9aab
Chris Kratz <Ckratz17@gmail.com>
2022-08-26 12:53:11 -0400

    set up queries and mutations in the utils to connect the front end to the back

--- Develop/client/src/pages/SavedBooks.js
+++ Develop/client/src/pages/SavedBooks.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
-import { getMe, deleteBook } from '../utils/API';
+import { useQuery, useMutation } from '@apollo/client'
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import { GET_ME } from '../utils/mutations'
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
