d9d9e74c1450109c4120121185e14fdd027289a0
Chris Kratz <Ckratz17@gmail.com>
2022-08-28 20:49:06 -0400

    all pages are rendering, user profiles are created and added to the DB, book results show when searched, however, they are not being saved to the user's profile

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import {
   ApolloClient,
   InMemoryCache,
@@ -6,7 +7,6 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
@@ -16,9 +16,7 @@ const httpLink = createHttpLink({
 });
 
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
   const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
@@ -38,20 +36,11 @@ function App() {
       <Router>
         <>
           <Navbar />
-            <Routes>
-              <Route 
-                path='/' 
-                element={<SearchBooks />} 
-              />
-              <Route 
-                path='/saved' 
-                element={<SavedBooks />} 
-              />
-              <Route 
-                path='*'
-                element={<h1 className='display-2'>Wrong page!</h1>}
-              />
-            </Routes>
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' element={<SavedBooks />} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
         </>
       </Router>
     </ApolloProvider>
